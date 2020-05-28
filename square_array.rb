35be1bc87209612c16d52245ba5509a5f306f6fc
Grace Kim <goooyoon@gmail.com>
2020-05-28 01:31:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,14 +7,7 @@ end
 end
 
 def square_array(array)
-array.each { |numbers| array << numbers ** 2 }
-end
-
-
-
-def square_array(array)
 squared = []
-end 	  
 array.each {|number| squared << number * number}
-  squared
+squared
 end
